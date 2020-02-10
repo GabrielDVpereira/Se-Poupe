e0bcd51fd563907239c541addb12dda63c790700
Gabriel Davi <gdavi.gd@gmail.com>
2020-02-09 22:10:11 -0300

    Configuring eslint and prettier

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -6,29 +6,28 @@ import {
   TouchableOpacity,
   Modal,
   TextInput,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
 } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { MaterialIcons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons, Entypo } from '@expo/vector-icons';
+
 import { SpendCards } from '../components/spendCards';
 import DatePicker from '../components/DatePicker';
 
 export const HomeScreen = () => {
-
-  const [spendModalVisible, setSpendModalVisible ] = useState(false);
-  const [spendingName, setSpendingName ] = useState('');
-  const [spendingValue, setSpendingValue ] = useState('');
-  const [spendingLocal, setSpendingLocal ] = useState('');
-  const [spendingDate, setSpendingDate ] = useState('');
-  const [spendCategory, setSpendingCategory ] = useState('');
+  const [spendModalVisible, setSpendModalVisible] = useState(false);
+  const [spendingName, setSpendingName] = useState('');
+  const [spendingValue, setSpendingValue] = useState('');
+  const [spendingLocal, setSpendingLocal] = useState('');
+  const [spendingDate, setSpendingDate] = useState('');
+  const [spendCategory, setSpendingCategory] = useState('');
   const [showDate, setShowDatePicker] = useState(false);
-  
+
   const showDatePicker = (show, date) => {
     setShowDatePicker(show);
-    setSpendingDate(date)
-  }
-  
+    setSpendingDate(date);
+  };
+
   return (
     <LinearGradient
       colors={['#6A0DAD', '#7029a3', '#9963bf']}
@@ -41,7 +40,11 @@ export const HomeScreen = () => {
 
       <View style={styles.content}>
         <View style={styles.contentTop}>
-          <TouchableOpacity onPress={() => {setSpendModalVisible(true)}}>
+          <TouchableOpacity
+            onPress={() => {
+              setSpendModalVisible(true);
+            }}
+          >
             <MaterialIcons name="add-box" size={32} color="#fff" />
           </TouchableOpacity>
           <TouchableOpacity style={styles.order}>
@@ -50,36 +53,73 @@ export const HomeScreen = () => {
           </TouchableOpacity>
         </View>
         <SpendCards />
-        
-          <Modal visible={spendModalVisible} transparent={true} animationType='slide'>
-            <View style={styles.modalContainer}>
-              <Text style={styles.modaltTitle}>Adicione um novo gasto  </Text>
-              <TextInput style={styles.input} placeholder={'Nome do gasto'} onChangeText ={(text) =>{setSpendingName(text)}}/>
-              <TextInput style={styles.input} placeholder={'Valor'} onChangeText ={(text) =>{setSpendingValue(text)}}/>
-              <TextInput style={styles.input} placeholder={'Local'} onChangeText ={(text) =>{setSpendingLocal(text)}}/>
-              <TextInput style={styles.input} placeholder={'Categoria'} onChangeText ={(text) =>{setSpendingCategory(text)}}/>
-              <TouchableOpacity style = {styles.datePicker} onPress={() => { showDatePicker(true) }}>
-                <Text>Data</Text>
-              </TouchableOpacity>
-                <View style={styles.modalButtons} >
-                  <KeyboardAvoidingView behavior="position" enabled>
-                      <TouchableOpacity style={styles.createButton} onPress={() => {
-                        console.log(spendingName)
-                        console.log(spendingValue)
-                        console.log(spendingLocal)
-                        console.log(spendingDate)
-                        console.log(spendCategory)
-                      }}>
-                        <Text>Criar</Text>
-                      </TouchableOpacity>
-                      <TouchableOpacity style={styles.cancelButton} onPress={() => {setSpendModalVisible(false)}}>
-                        <Text>Cancelar</Text>
-                      </TouchableOpacity>
-                  </KeyboardAvoidingView>
-                </View>
+
+        <Modal visible={spendModalVisible} transparent animationType="slide">
+          <View style={styles.modalContainer}>
+            <Text style={styles.modaltTitle}>Adicione um novo gasto </Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nome do gasto"
+              onChangeText={text => {
+                setSpendingName(text);
+              }}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Valor"
+              onChangeText={text => {
+                setSpendingValue(text);
+              }}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Local"
+              onChangeText={text => {
+                setSpendingLocal(text);
+              }}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Categoria"
+              onChangeText={text => {
+                setSpendingCategory(text);
+              }}
+            />
+            <TouchableOpacity
+              style={styles.datePicker}
+              onPress={() => {
+                showDatePicker(true);
+              }}
+            >
+              <Text>Data</Text>
+            </TouchableOpacity>
+            <View style={styles.modalButtons}>
+              <KeyboardAvoidingView behavior="position" enabled>
+                <TouchableOpacity
+                  style={styles.createButton}
+                  onPress={() => {
+                    console.log(spendingName);
+                    console.log(spendingValue);
+                    console.log(spendingLocal);
+                    console.log(spendingDate);
+                    console.log(spendCategory);
+                  }}
+                >
+                  <Text>Criar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.cancelButton}
+                  onPress={() => {
+                    setSpendModalVisible(false);
+                  }}
+                >
+                  <Text>Cancelar</Text>
+                </TouchableOpacity>
+              </KeyboardAvoidingView>
             </View>
-          </Modal>
-          <DatePicker show ={showDate} showDatePicker ={showDatePicker}/>
+          </View>
+        </Modal>
+        <DatePicker show={showDate} showDatePicker={showDatePicker} />
       </View>
     </LinearGradient>
   );
@@ -122,31 +162,30 @@ const styles = StyleSheet.create({
   order: {
     flexDirection: 'row',
   },
-  modalContainer:{
+  modalContainer: {
     width: 300,
-    height: 400, 
-    padding: 20, 
-    backgroundColor: "#fff", 
+    height: 400,
+    padding: 20,
+    backgroundColor: '#fff',
     alignSelf: 'center',
-    top: 100, 
-    elevation: 15
+    top: 100,
+    elevation: 15,
   },
-  modaltTitle:{
-    alignSelf: 'center'
+  modaltTitle: {
+    alignSelf: 'center',
   },
-  input:{
-    top:30,
+  input: {
+    top: 30,
     marginVertical: 5,
     borderBottomWidth: 1,
-    width: 200
+    width: 200,
   },
-  modalButtons:{
-    flex: 1, 
-    alignItems: 'center', 
-    flexDirection: 'row'
+  modalButtons: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   datePicker: {
-    top: 40
-  }
-
+    top: 40,
+  },
 });
