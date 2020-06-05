5bb5d375c693800d2efef25a44cb563283220a11
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-05 20:54:57 -0300

    Refactor routes

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
-import { Routes } from './src/Routes';
+import Routes from './src/routes/index.routes';
 import AuthContextProvider from './src/contexts/AuthContext';
 
 export default function App() {
