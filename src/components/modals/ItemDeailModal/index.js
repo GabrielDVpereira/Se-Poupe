f7e7ee52e5346f5d63b2e590c25ee22887464537
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-28 20:55:38 -0300

    Starte filter modal animation

--- src/components/modals/ItemDeailModal/index.js
+++ src/components/modals/ItemDeailModal/index.js
@@ -1,121 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import {
-  Modal,
-  TouchableWithoutFeedback,
-  Animated,
-  Dimensions,
-} from 'react-native';
-import { PanGestureHandler, State } from 'react-native-gesture-handler';
-import {
-  Conatainer,
-  Content,
-  Title,
-  Price,
-  Category,
-  Date,
-  Name,
-  Value,
-  Drawer,
-  DrawerLine,
-} from './styles';
+import React from 'react';
+
+import { Title, Price, Category, Date, Name, Value } from './styles';
 import Button from '../../Buttom';
-
-const { height: screenHeight } = Dimensions.get('window');
-
-const AnimatedContent = Animated.createAnimatedComponent(Content);
-const modalTopDistance = screenHeight - screenHeight * 0.6;
-const top = new Animated.Value(modalTopDistance);
-const translationY = new Animated.Value(0);
+import ModalAnimated from '../ModalAnimated';
 
 export default function ItemModal({ item, visible, setVisible }) {
-  const [containerStyle, SetContainerStyle] = useState();
-  const [offsetY, setOffsetY] = useState(0);
-
-  function getContainerStyle() {
-    if (visible) {
-      return {
-        zIndex: 3,
-        backgroundColor: 'rgba(0,0,0,0.3)',
-      };
-    }
-    return {
-      zIndex: 0,
-      backgroundColor: '#fff',
-    };
-  }
-
-  useEffect(() => {
-    if (visible) {
-      SetContainerStyle(getContainerStyle());
-      Animated.spring(top, {
-        toValue: modalTopDistance,
-      }).start();
-    } else {
-      Animated.timing(top, {
-        toValue: screenHeight,
-        duration: 200,
-      }).start(() => {
-        SetContainerStyle(getContainerStyle());
-        translationY.setValue(0);
-      });
-    }
-  }, [visible]);
-
   return (
-    <>
-      <Conatainer onPress={() => setVisible(false)} style={containerStyle}>
-        <TouchableWithoutFeedback>
-          <AnimatedContent
-            style={{
-              top,
-              transform: [
-                {
-                  translateY: translationY.interpolate({
-                    inputRange: [0, screenHeight],
-                    outputRange: [0, screenHeight],
-                    extrapolate: 'clamp',
-                  }),
-                },
-              ],
-            }}
-          >
-            <PanGestureHandler
-              onGestureEvent={({ nativeEvent }) => {
-                translationY.setValue(nativeEvent.translationY + offsetY);
-              }}
-              onHandlerStateChange={({ nativeEvent }) => {
-                if (
-                  nativeEvent.oldState === State.ACTIVE &&
-                  nativeEvent.translationY > 30
-                ) {
-                  setVisible(false);
-                  setOffsetY(0);
-                } else {
-                  setOffsetY(nativeEvent.translationY + offsetY);
-                }
-              }}
-            >
-              <Drawer>
-                <DrawerLine />
-              </Drawer>
-            </PanGestureHandler>
-            <Title>LG ultraGrear</Title>
-            <Price>
-              <Name>Preço</Name>
-              <Value>R$ 720</Value>
-            </Price>
-            <Category>
-              <Name>Categoria</Name>
-              <Value>Eletronicos</Value>
-            </Category>
-            <Date>
-              <Name>Data</Name>
-              <Value>17/02/2020</Value>
-            </Date>
-            <Button type="danger" text="Remover item" />
-          </AnimatedContent>
-        </TouchableWithoutFeedback>
-      </Conatainer>
-    </>
+    <ModalAnimated visible={visible} setVisible={setVisible}>
+      <Title>LG ultraGrear</Title>
+      <Price>
+        <Name>Preço</Name>
+        <Value>R$ 720</Value>
+      </Price>
+      <Category>
+        <Name>Categoria</Name>
+        <Value>Eletronicos</Value>
+      </Category>
+      <Date>
+        <Name>Data</Name>
+        <Value>17/02/2020</Value>
+      </Date>
+      <Button type="danger" text="Remover item" />
+    </ModalAnimated>
   );
 }
--- src/components/modals/ItemDeailModal/styles.js
+++ src/components/modals/ItemDeailModal/styles.js
@@ -1,29 +1,4 @@
 import styled, { css } from 'styled-components/native';
-import { Dimensions } from 'react-native';
-
-const { height: screenHeight } = Dimensions.get('window');
-
-export const Conatainer = styled.TouchableOpacity.attrs({
-  activeOpacity: 1,
-})`
-  height: 100%;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  position: absolute;
-  z-index: 3;
-`;
-export const Content = styled.View`
-  width: 100%;
-  height: ${screenHeight * 0.65}px;
-  background-color: #fff;
-  border-top-left-radius: 30px;
-  border-top-right-radius: 30px;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px 20px 70px 20px;
-  position: absolute;
-  z-index: 3;
-`;
 
 export const Title = styled.Text`
   font-family: 'Montserrat_700Bold';
@@ -60,18 +35,4 @@ export const Value = styled.Text`
   font-family: 'Montserrat_500Medium';
   font-size: 16px;
   color: #ccc;
-`;
-
-export const Drawer = styled.View`
-  width: 100%;
-  height: 40px;
-  position: absolute;
-  align-items: center;
-`;
-export const DrawerLine = styled.View`
-  margin-top: 15px;
-  height: 10%;
-  width: 40%;
-  background-color: #ccc;
-  border-radius: 20px;
 `;
