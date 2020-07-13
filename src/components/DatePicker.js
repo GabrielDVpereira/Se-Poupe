a0063a3fd9f4597037788c5b6099a392881e6f90
Gabriel Davi <gdavi.gd@gmail.com>
2020-07-12 22:33:00 -0300

    Config datepicker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Text } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import moment from 'moment';
 
-export default function DatePicker({ show, showDatePicker }) {
+export default function DatePicker({ onChange }) {
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  console.log(datePickerVisible);
+  useEffect(() => {
+    onChange(selectedDate);
+  }, [selectedDate]);
+
   return (
-    <View>
-      <TouchableOpacity onPress={() => {}}>
-        {show && (
-          <DateTimePicker
-            testID="dateTimePicker"
-            timeZoneOffsetInMinutes={0}
-            value={new Date()}
-            mode="date"
-            is24Hour
-            display="calendar"
-            onChange={(event, selectedDate) => {
-              showDatePicker(false, moment(selectedDate).format('DD/MM/YYYY'));
-            }}
-          />
-        )}
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        setDatePickerVisible(true);
+      }}
+      style={{ width: 100 }}
+    >
+      <Text>{selectedDate || 'Data'}</Text>
+      {datePickerVisible && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          timeZoneOffsetInMinutes={0}
+          value={new Date()}
+          mode="date"
+          is24Hour
+          display="calendar"
+          onChange={(event, selectedDate) => {
+            setSelectedDate(moment(selectedDate).format('DD/MM/YYYY'));
+            setDatePickerVisible(false);
+          }}
+        />
+      )}
+    </TouchableOpacity>
   );
 }
--- src/components/modals/FilterModal/index.js
+++ src/components/modals/FilterModal/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 import ModalAnimated from '../ModalAnimated';
 import PriceRangeSelector from './PriceRangeSelector';
-
+import Picker from '../../Picker';
+import categories from '../../../utils/categories';
+import Buttom from '../../Buttom';
 import { Title } from './styles';
+import DatePicker from '../../DatePicker';
+
+const categoryNames = Object.keys(categories);
 
 export default function({ visible, setVisible }) {
   return (
     <ModalAnimated visible={visible} setVisible={setVisible}>
       <Title>Filtrar</Title>
       <PriceRangeSelector />
+      <Picker
+        label="Categoria"
+        items={categoryNames}
+        onChange={item => console.log(item)}
+      />
+      <DatePicker onChange={date => {}} />
+      <Buttom text="Aplicar" />
     </ModalAnimated>
   );
 }
