fd493df424df0bd414f2913ff96ed20c1e5c9c40
Gabriel Davi <gdavi.gd@gmail.com>
2020-07-28 23:50:22 -0300

    fix date picker error

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Entypo } from '@expo/vector-icons';
+import moment from 'moment';
 import { DatePickerContainer, Label } from './styles';
 import getFormatedDateFromTimestamps from '../../utils/getFormatedDateFromTimestamps';
 
@@ -8,10 +9,8 @@ export default function DatePicker({ onChange }) {
   const [datePickerVisible, setDatePickerVisible] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
 
-  console.log(datePickerVisible);
   useEffect(() => {
     onChange(selectedDate);
-    setDatePickerVisible(false);
   }, [selectedDate]);
 
   return (
@@ -20,18 +19,19 @@ export default function DatePicker({ onChange }) {
         setDatePickerVisible(true);
       }}
     >
-      <Label active={!!selectedDate}>{selectedDate || 'Data'}</Label>
+      <Label active={!!selectedDate}>
+        {(selectedDate && moment(selectedDate).format('DD/MM/YYYY')) || 'Data'}
+      </Label>
       {datePickerVisible && (
         <DateTimePicker
           testID="dateTimePicker"
           timeZoneOffsetInMinutes={0}
-          value={(selectedDate && new Date(selectedDate)) || new Date()}
+          value={selectedDate || new Date()}
           mode="date"
           is24Hour
           display="calendar"
           onChange={(event, date) => {
-            const formatedDate = getFormatedDateFromTimestamps(date);
-            console.log(formatedDate);
+            setDatePickerVisible(false);
             setSelectedDate(date);
           }}
         />
